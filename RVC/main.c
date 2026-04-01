baaed1eda135aa7846e0b01f12e7e26a299bac3f
William <viliamoleosdev@gmail.com>
2026-04-01 22:40:37 +0500

    main.c*>[rv_openFile()]

--- RVC/main.c
+++ RVC/main.c
@@ -21,12 +21,30 @@
 	SetConsoleMode(console, consoleState); \
 }
 
+////////////////////////////////////////// FILE //////////////////////////////////////////
+
+char* rv_openFile(const char* path) {
+	OFSTRUCT openstruct; 
+	HFILE* nufile = malloc(sizeof(HFILE)+1); // HEDER + NULL IN BUF
+
+	*nufile = OpenFile(
+		path, &openstruct,
+		OF_READ | OF_SHARE_DENY_READ
+	);
+	*(char*)(nufile+1) = '\0';
+
+	return((char*)nufile);
+}
+
 ////////////////////////////////////////// MAIN //////////////////////////////////////////
 
 int main() {
+	char* mainfile;
+
 	CC_ENABLECOLOURCONSOLE();
 
-	printf("\e[35mHello, World!\e[0m\n");
+	mainfile = rv_openFile("main.c");
+	printf("Opened file main.c: %X {!=%X}", *(HFILE*)mainfile, HFILE_ERROR);
 
 	return(0);
 }
