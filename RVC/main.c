0f9feafc781af3287ac50b839b9b3abde905ab87
William <viliamoleosdev@gmail.com>
2026-05-13 01:31:35 +0500

    main.c*>(reject humanity, go back to monke)

--- RVC/main.c
+++ RVC/main.c
@@ -29,81 +29,17 @@
 
 #define CC_RED "\e[31m"
 
-//////////////////////////////////// ARGV HOMOGENISER ////////////////////////////////////
-
-/// dest is a pointer to string 'cause weird stuff, just didn't bother
-void argv_homogeniser(char** dest, int argc, char** argv) {
-	int i; char* p;
-	uint16_t sumarglen;
-	uint16_t argvlens[argc];
-
-	if(argc==1) { *dest=""; } else {
-
-		for(sumarglen=0, i=1; i<argc; i++) { argvlens[i]=strlen(argv[i]); sumarglen+=argvlens[i]+1; }
-
-		*dest = malloc(sumarglen);
-
-		for(p=*dest,i=1; i<argc; i++) {
-			memcpy(p, argv[i], argvlens[i]);
-			p += argvlens[i];
-			*p = ' '; p++;
-		} p[-1] = '\0';
-
-	}
-}
-
 ////////////////////////////////////////// MAIN //////////////////////////////////////////
 
-#define EXFILTRATE(...) \
-	printf(__VA_ARGS__); goto exfiltration;
-
-#define IL_FDASHARG() do { \
-		while((0<*p)&&(*p<33)) { p++; } /* skip whitespace */ \
-		switch(*p) { \
-			case '\'': \
-				printf("string!\n"); \
-			break; \
-			 \
-			default: \
-				for(b=p; !(*b<33); b++) { } \
-				curargcontent = malloc(b-p+1); \
-				memcpy(curargcontent, p, b-p); curargcontent[b-p]='\0'; \
-				printf("RESULT: \"%s\"\n", curargcontent);
-			break; \
-		} \
-	} while(0)
-
-#define IL_DASHARGS() do { \
-		p+=2; \
-		switch(p[-1]) { \
-			case 'f': \
-				IL_FDASHARG(); \
-			break; \
-			 \
-			default: \
-				EXFILTRATE(CC_RED "Unrecognised dash argument -%c.\n" CC_NULL, p[-1]); \
-			break; \
-		} \
-	} while(0)
-
 int main(int argc, char* argv[]) {
-	char* p; char* b;
-	char* args;
-	char* curargcontent;
+	char** argvend;
 
 		CC_ENABLECOLOURCONSOLE();
 
-	argv_homogeniser(&args, argc, argv);
-
-	for(p=args; *p!='\0'; p++) {
-		switch(*p) {
-			case '-':
-				IL_DASHARGS();
-			break;
-		}
+	for(argvend=argv+argc; argv<argvend; argv++) {
+		printf("\"%s\" ", *argv);
 	}
-	
-exfiltration:
+
 	return(0);
 }
 
