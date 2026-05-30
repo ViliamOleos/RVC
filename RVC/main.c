038e166e8e755abc83b48e03a70bffcd15c7c0d4
William <viliamoleosdev@gmail.com>
2026-05-31 03:05:49 +0500

    main.c*>(cmdparse start); -baumeister.cmd>(preparing for installable rvbatchutils)

--- RVC/main.c
+++ RVC/main.c
@@ -31,15 +31,75 @@
 
 ////////////////////////////////////////// MAIN //////////////////////////////////////////
 
+/// 64 (0 through 63) flags
+uint64_t flags = 0;
+#define FLAGS_SETTRUE(flag) do { flags |= 1<<(flag); } while(0)
+#define FLAGS_GET(flag) ((flags>>(flag))&1)
+#define FLAGS_HELP 0
+
 int main(int argc, char* argv[]) {
+	char* p; char* b;
 	char** argvend;
+	char* argdat;
+	uint16_t strsiz;
+
+	argdat = NULL;
 
 		CC_ENABLECOLOURCONSOLE();
 
-	for(argvend=argv+argc; argv<argvend; argv++) {
-		printf("\"%s\" ", *argv);
+	for(argvend=(argv++)+argc; argv<argvend; argv++) {
+		switch(*(p=*argv)) {
+			case '-':
+				switch(*(++p)) {
+					case 'h':
+						FLAGS_SETTRUE(FLAGS_HELP);
+					break; case '-':
+						for(b=p; *b!='\0'; b++) { }
+						argdat = realloc(argdat, b-p);
+						memcpy(argdat, p+1, b-p);
+
+						if(!strcmp(argdat, "help")) {
+							FLAGS_SETTRUE(FLAGS_HELP);
+						}
+					break; default:
+						printf(CC_RED"Unrecognised dash argument \"-%c\"\n"CC_NULL, *p);
+						goto exfiltration;
+					break;
+				}
+			break; case '~':
+				switch(*(++p)) {
+					case 'f':
+						argv++; p=*argv;
+						strsiz = strlen(p);
+						argdat = realloc(argdat, strsiz+1);
+						memcpy(argdat, p, strsiz+1);
+						printf("\targdat = \"%s\"\n", argdat);
+					break; case '~':
+						for(b=p; *b!='\0'; b++) { }
+						argdat = realloc(argdat, b-p);
+						memcpy(argdat, p+1, b-p);
+
+						if(!strcmp(argdat, "file")) {
+							argv++; p=*argv;
+							strsiz = strlen(p);
+							argdat = realloc(argdat, strsiz+1);
+							memcpy(argdat, p, strsiz+1);
+							printf("\targdat = \"%s\"\n", argdat);
+						}
+					break; default:
+						printf(CC_RED"Unrecognised tilde argument \"~%c\"\n"CC_NULL, *p);
+						goto exfiltration;
+					break;
+				}
+			break;
+		}
 	}
 
+	if(FLAGS_GET(FLAGS_HELP)) {
+		printf("Hello, World!\n");
+	}
+
+exfiltration:
 	return(0);
 }
 
